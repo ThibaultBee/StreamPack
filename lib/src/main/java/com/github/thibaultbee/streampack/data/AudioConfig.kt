bdad6f0e8b8c76a9a5b8867d474fe69f942e2ea4
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2021-03-04 21:51:17 +0100

    feat(Config): Check if config mime type is audio or video

--- lib/src/main/java/com/github/thibaultbee/streampack/data/AudioConfig.kt
+++ lib/src/main/java/com/github/thibaultbee/streampack/data/AudioConfig.kt
@@ -1,6 +1,7 @@
 package com.github.thibaultbee.streampack.data
 
 import android.media.AudioFormat
+import com.github.thibaultbee.streampack.utils.isAudio
 import java.security.InvalidParameterException
 
 data class AudioConfig(
@@ -10,6 +11,10 @@ data class AudioConfig(
     var channelConfig: Int,
     var audioByteFormat: Int
 ) {
+    init {
+        require(mimeType.isAudio())
+    }
+
     companion object {
         fun getChannelNumber(channelConfig: Int) = when (channelConfig) {
             AudioFormat.CHANNEL_IN_MONO -> 1
--- lib/src/main/java/com/github/thibaultbee/streampack/data/VideoConfig.kt
+++ lib/src/main/java/com/github/thibaultbee/streampack/data/VideoConfig.kt
@@ -1,11 +1,16 @@
 package com.github.thibaultbee.streampack.data
 
 import android.util.Size
+import com.github.thibaultbee.streampack.utils.isVideo
 
 data class VideoConfig(
     var mimeType: String,
     var startBitrate: Int,
     var resolution: Size,
     var fps: Int
-)
+) {
+    init {
+        require(mimeType.isVideo())
+    }
+}
 
