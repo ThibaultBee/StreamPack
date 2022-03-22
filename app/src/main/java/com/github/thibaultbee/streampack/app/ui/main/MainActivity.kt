7784ed9d9b5583f28af6b6b27a06420233f55920
ThibaultBee <thibault@api.video>
2022-03-22 11:05:06 +0100

    refactor(app): remove rx dependencies

--- app/src/main/java/com/github/thibaultbee/streampack/app/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/thibaultbee/streampack/app/ui/main/MainActivity.kt
@@ -25,13 +25,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.github.thibaultbee.streampack.app.R
 import com.github.thibaultbee.streampack.app.databinding.MainActivityBinding
 import com.github.thibaultbee.streampack.app.ui.settings.SettingsActivity
-import com.jakewharton.rxbinding4.view.clicks
-import com.jakewharton.rxbinding4.widget.itemClicks
-import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 
 class MainActivity : AppCompatActivity() {
-    private val activityDisposables = CompositeDisposable()
     private lateinit var binding: MainActivityBinding
     private val tag = this::class.java.simpleName
 
@@ -50,11 +46,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun bindProperties() {
-        binding.actions.clicks()
-            .subscribe {
-                showPopup()
-            }
-            .let(activityDisposables::add)
+        binding.actions.setOnClickListener {
+            showPopup()
+        }
     }
 
     private fun showPopup() {
@@ -62,11 +56,13 @@ class MainActivity : AppCompatActivity() {
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.actions, popup.menu)
         popup.show()
-        popup.itemClicks().subscribe {
+        popup.setOnMenuItemClickListener {
             if (it.itemId == R.id.action_settings) {
                 goToSettingsActivity()
+                true
             } else {
                 Log.e(tag, "Unknown menu item ${it.itemId}")
+                false
             }
         }
     }
@@ -76,14 +72,8 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.actions, menu)
         return true
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        activityDisposables.clear()
     }
 }
