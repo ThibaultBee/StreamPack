2bfe71d0bbe4d8eaa10fd846c814f6d4c5ef9270
ThibaultBee <thibault@api.video>
2022-03-23 11:26:40 +0100

    refactor(*): arrange folder and split library to core, extension-rtmp and extension-srt

---
+++ demos/camera/src/main/java/com/github/thibaultbee/streampack/app/ui/BindingAdapters.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2022 Thibault B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.thibaultbee.streampack.app.ui
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
+import com.google.android.material.slider.Slider
+
+object BindingAdapters {
+    @BindingAdapter("camera:goneUnless")
+    @JvmStatic
+    fun goneUnless(view: View, visible: Boolean) {
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+}
+
+@InverseBindingAdapter(attribute = "android:value")
+fun getSliderValue(slider: Slider) = slider.value
+
+@BindingAdapter("android:valueAttrChanged")
+fun setSliderListeners(slider: Slider, attrChange: InverseBindingListener) {
+    slider.addOnChangeListener { _, _, _ ->
+        attrChange.onChange()
+    }
+}
---
+++ extensions/rtmp/src/main/java/com/github/thibaultbee/streampack/ext/rtmp/internal/endpoints/RtmpProducer.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2022 Thibault B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.thibaultbee.streampack.ext.rtmp.internal.endpoints
+
+import com.github.thibaultbee.streampack.internal.data.Packet
+import com.github.thibaultbee.streampack.internal.endpoints.IEndpoint
+import com.github.thibaultbee.streampack.listeners.OnConnectionListener
+import com.github.thibaultbee.streampack.logger.ILogger
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import video.api.rtmpdroid.Rtmp
+
+class RtmpProducer(
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
+    val logger: ILogger
+) :
+    IEndpoint {
+    var onConnectionListener: OnConnectionListener? = null
+
+    private var socket = Rtmp()
+
+    override fun configure(startBitrate: Int) {
+    }
+
+    suspend fun connect(url: String) = withContext(coroutineDispatcher) {
+        try {
+            socket.connect("$url live=1")
+            onConnectionListener?.onSuccess()
+        } catch (e: Exception) {
+            socket = Rtmp()
+            onConnectionListener?.onFailed(e.message ?: "Unknown error")
+            throw e
+        }
+    }
+
+    fun disconnect() {
+        socket.close()
+        socket = Rtmp()
+    }
+
+    override fun write(packet: Packet) {
+        socket.write(packet.buffer)
+    }
+
+    override fun startStream() {
+        socket.connectStream()
+    }
+
+    override fun stopStream() {
+        socket.deleteStream()
+    }
+
+    override fun release() {
+    }
+}
