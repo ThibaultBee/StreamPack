17c54c4c3bb12c6b8864d787c79623ede10793f8
ThibaultBee <thibault@api.video>
2022-08-14 16:21:55 +0200

    feat(demos): add a STOP button in screen recorder notification

--- demos/screenrecorder/src/main/java/io/github/thibaultbee/streampack/screenrecorder/services/DemoScreenRecorderRtmpLiveService.kt
+++ demos/screenrecorder/src/main/java/io/github/thibaultbee/streampack/screenrecorder/services/DemoScreenRecorderRtmpLiveService.kt
@@ -15,11 +15,46 @@
  */
 package io.github.thibaultbee.streampack.screenrecorder.services
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.core.app.NotificationCompat
 import io.github.thibaultbee.streampack.ext.rtmp.services.ScreenRecorderRtmpLiveService
 import io.github.thibaultbee.streampack.screenrecorder.R
+import io.github.thibaultbee.streampack.screenrecorder.models.Actions
+
 
 class DemoScreenRecorderRtmpLiveService : ScreenRecorderRtmpLiveService(
     notificationId = 0x4568,
     channelId = "io.github.thibaultbee.streampack.screenrecorder.demo.rtmp",
     channelNameResourceId = R.string.app_name
-)
+) {
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        streamer?.let {
+            if (intent.action == Actions.STOP.value) {
+                streamer?.stopStream()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onConnectionSuccessNotification(): Notification {
+        val intent =
+            Intent(
+                this,
+                DemoScreenRecorderRtmpLiveService::class.java
+            ).setAction(Actions.STOP.value)
+        val stopIntent =
+            PendingIntent.getService(this, 5678, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        return NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(notificationIconResourceId)
+            .setContentTitle(getString(R.string.live_in_progress))
+            .addAction(
+                R.drawable.ic_baseline_stop_24,
+                getString(R.string.stop),
+                stopIntent
+            )
+            .build()
+    }
+}
--- demos/screenrecorder/src/main/java/io/github/thibaultbee/streampack/screenrecorder/services/DemoScreenRecorderSrtLiveService.kt
+++ demos/screenrecorder/src/main/java/io/github/thibaultbee/streampack/screenrecorder/services/DemoScreenRecorderSrtLiveService.kt
@@ -15,11 +15,42 @@
  */
 package io.github.thibaultbee.streampack.screenrecorder.services
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import androidx.core.app.NotificationCompat
 import io.github.thibaultbee.streampack.ext.srt.services.ScreenRecorderSrtLiveService
 import io.github.thibaultbee.streampack.screenrecorder.R
+import io.github.thibaultbee.streampack.screenrecorder.models.Actions
 
 class DemoScreenRecorderSrtLiveService : ScreenRecorderSrtLiveService(
     notificationId = 0x4569,
     channelId = "io.github.thibaultbee.streampack.screenrecorder.demo.srt",
     channelNameResourceId = R.string.app_name
-)
+) {
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        streamer?.let {
+            if (intent.action == Actions.STOP.value) {
+                streamer?.stopStream()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onConnectionSuccessNotification(): Notification {
+        val intent =
+            Intent(this, DemoScreenRecorderSrtLiveService::class.java).setAction(Actions.STOP.value)
+        val stopIntent =
+            PendingIntent.getService(this, 5678, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        return NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(notificationIconResourceId)
+            .setContentTitle(getString(R.string.live_in_progress))
+            .addAction(
+                R.drawable.ic_baseline_stop_24,
+                getString(R.string.stop),
+                stopIntent
+            )
+            .build()
+    }
+}
