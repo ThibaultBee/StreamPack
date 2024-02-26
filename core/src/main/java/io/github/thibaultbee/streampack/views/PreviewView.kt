8454ee995552c2a2e43c15df3fea45c9b2266a3d
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-02-26 18:55:23 +0100

    feat(core): add way to scale the preview view inside its parent It also fixes issue with the aspect ratio and the preview

--- core/src/main/java/io/github/thibaultbee/streampack/views/PreviewView.kt
+++ core/src/main/java/io/github/thibaultbee/streampack/views/PreviewView.kt
@@ -21,13 +21,23 @@ import android.content.pm.PackageManager
 import android.graphics.PointF
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Size
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
+import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.ViewConfiguration
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.camera.viewfinder.CameraViewfinder
+import androidx.camera.viewfinder.CameraViewfinder.ScaleType
+import androidx.camera.viewfinder.ViewfinderSurfaceRequest
+import androidx.camera.viewfinder.populateFromCharacteristics
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.FutureCallback
+import com.google.common.util.concurrent.Futures
 import io.github.thibaultbee.streampack.R
 import io.github.thibaultbee.streampack.logger.Logger
 import io.github.thibaultbee.streampack.streamers.interfaces.ICameraStreamer
@@ -37,42 +47,82 @@ import io.github.thibaultbee.streampack.utils.frontCameraList
 import io.github.thibaultbee.streampack.utils.getCameraCharacteristics
 
 /**
- * A [FrameLayout] containing a [AutoFitSurfaceView] that manages [ICameraStreamer] preview.
- * In the case, you are using it, do not call [ICameraStreamer.startPreview] or
- * [ICameraStreamer.stopPreview] on application side.
+ * A [FrameLayout] containing a preview for the [ICameraStreamer].
  *
- * The [Manifest.permission.CAMERA] permission must be granted before using this class.s
+ * It handles the display, the aspect ratio and the scaling of the preview.
+ *
+ * In the case, you are using it, do not call [ICameraStreamer.startPreview] or
+ * [ICameraStreamer.stopPreview] on application side. It will be handled by the [PreviewView].
+ *
+ * The [Manifest.permission.CAMERA] permission must be granted before using this view.
  */
 class PreviewView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : FrameLayout(context, attrs, defStyle) {
-    private val surfaceView = AutoFitSurfaceView(context)
-    private val cameraFacingDirection: FacingDirection
+    private val cameraViewFinder = CameraViewfinder(context, attrs, defStyle)
+    private var viewFinderSurfaceRequest: ViewfinderSurfaceRequest? = null
+
+    private val cameraFacingDirection: CameraFacingDirection
     private val defaultCameraId: String?
 
+    private var previewState = PreviewState.STOPPED
+
+    /**
+     * Enables zoom on pinch gesture.
+     */
     var enableZoomOnPinch: Boolean
+
+    /**
+     * Enables tap to focus.
+     */
     var enableTapToFocus: Boolean
 
-    private var touchUpEvent: MotionEvent? = null
-
+    /**
+     * Sets the [ICameraStreamer] to preview.
+     * Once set, the [PreviewView] will try to start the preview.
+     *
+     * Only one [ICameraStreamer] can be set.
+     */
     var streamer: ICameraStreamer? = null
         /**
-         * Set the [ICameraStreamer] to use.
+         * Sets the [ICameraStreamer] to preview.
          *
-         * @param value the [ICameraStreamer] to use
+         * @param value the [ICameraStreamer] to preview
          */
         set(value) {
-            streamer?.stopPreview()
-            field = value
-            startPreviewIfReady()
+            post {
+                stopPreviewInternal()
+                field = value
+                startPreviewInternal(size)
+            }
+        }
+
+    /**
+     * The position of the [PreviewView] within its container.
+     */
+    var position: Position
+        get() = getPosition(cameraViewFinder.scaleType)
+        set(value) {
+            cameraViewFinder.scaleType = getScaleType(scaleMode, value)
+        }
+
+    /**
+     * The scale mode of the [PreviewView] within its container.
+     */
+    var scaleMode: ScaleMode
+        get() = getScaleMode(cameraViewFinder.scaleType)
+        set(value) {
+            cameraViewFinder.scaleType = getScaleType(value, position)
         }
 
     /**
      * The [Listener] to listen to specific view events.
      */
     var listener: Listener? = null
+
+    private var touchUpEvent: MotionEvent? = null
 
     private val pinchGesture = ScaleGestureDetector(
         context,
@@ -83,16 +133,16 @@ class PreviewView @JvmOverloads constructor(
         val a = context.obtainStyledAttributes(attrs, R.styleable.PreviewView)
 
         try {
-            cameraFacingDirection = FacingDirection.fromValue(
-                a.getString(R.styleable.PreviewView_cameraFacingDirection)
-                    ?: DEFAULT_CAMERA_FACING.value
+            cameraFacingDirection = CameraFacingDirection.entryOf(
+                a.getInt(R.styleable.PreviewView_cameraFacingDirection, DEFAULT_CAMERA_FACING.value)
             )
+
             defaultCameraId = when (cameraFacingDirection) {
-                FacingDirection.FRONT -> {
+                CameraFacingDirection.FRONT -> {
                     context.frontCameraList.firstOrNull()
                 }
 
-                FacingDirection.BACK -> {
+                CameraFacingDirection.BACK -> {
                     context.backCameraList.firstOrNull()
                 }
             }
@@ -101,14 +151,41 @@ class PreviewView @JvmOverloads constructor(
                 a.getBoolean(R.styleable.PreviewView_enableZoomOnPinch, true)
             enableTapToFocus =
                 a.getBoolean(R.styleable.PreviewView_enableTapToFocus, true)
+
+            scaleMode = ScaleMode.entryOf(
+                a.getInt(
+                    R.styleable.PreviewView_scaleMode,
+                    ScaleMode.FILL.value
+                )
+            )
+            position = Position.entryOf(
+                a.getInt(
+                    R.styleable.PreviewView_position,
+                    Position.CENTER.value
+                )
+            )
+
         } finally {
             a.recycle()
         }
 
-        surfaceView.holder.addCallback(StreamerHolderCallback())
-        addView(surfaceView)
+        addView(
+            cameraViewFinder,
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        startPreviewIfReady(size, true)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        stopPreview()
+    }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (streamer == null) {
@@ -136,7 +213,7 @@ class PreviewView @JvmOverloads constructor(
     }
 
     override fun performClick(): Boolean {
-        streamer?.let { it ->
+        streamer?.let {
             if (enableTapToFocus) {
                 // mTouchUpEvent == null means it's an accessibility click. Focus at the center instead.
                 val x = touchUpEvent?.x ?: (width / 2f)
@@ -153,65 +230,202 @@ class PreviewView @JvmOverloads constructor(
         return super.performClick()
     }
 
-    private fun startPreviewIfReady(shouldFailSilently: Boolean = false) {
-        if (display != null) {
-            streamer?.let {
-                try {
-                    val camera = defaultCameraId ?: it.camera
-                    Logger.i(TAG, "Starting on camera: $camera")
+    /**
+     * Stops the preview.
+     */
+    private fun stopPreview() {
+        post {
+            stopPreviewInternal()
+        }
+    }
 
-                    // Selects appropriate preview size
-                    val previewSize = getPreviewOutputSize(
-                        this.display,
-                        context.getCameraCharacteristics(camera),
-                        SurfaceHolder::class.java
-                    )
-                    Logger.d(
-                        TAG,
-                        "View finder size: $width x $height"
-                    )
-                    Logger.d(TAG, "Selected preview size: $previewSize")
-                    surfaceView.setAspectRatio(previewSize.width, previewSize.height)
+    private fun stopPreviewInternal() {
+        streamer?.stopPreview()
+        viewFinderSurfaceRequest?.markSurfaceSafeToRelease()
+        viewFinderSurfaceRequest = null
+        previewState = PreviewState.STOPPED
+    }
 
-                    // To ensure that size is set, initialize camera in the view's thread
-                    post {
-                        if (ActivityCompat.checkSelfPermission(
-                                context,
-                                Manifest.permission.CAMERA
-                            ) != PackageManager.PERMISSION_GRANTED
-                        ) {
-                            throw SecurityException("Camera permission is needed to run this application")
-                        }
-                        it.startPreview(surfaceView, camera)
-                        listener?.onPreviewStarted()
-                    }
-                } catch (e: Exception) {
-                    if (shouldFailSilently) {
-                        Logger.w(TAG, e.toString(), e)
-                    } else {
-                        throw e
-                    }
-                }
+    /**
+     * Starts the preview if the view size is ready.
+     *
+     * @param targetViewSize the view size
+     * @param shouldFailSilently true to fail silently
+     */
+    private fun startPreviewIfReady(targetViewSize: Size, shouldFailSilently: Boolean) {
+        try {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                throw SecurityException("Camera permission is needed to run this application")
+            }
+
+            post {
+                startPreviewInternal(targetViewSize)
+            }
+        } catch (e: Exception) {
+            if (shouldFailSilently) {
+                Logger.w(TAG, e.toString(), e)
+            } else {
+                throw e
             }
         }
     }
 
-    companion object {
-        private const val TAG = "PreviewView"
-        
-        private val DEFAULT_CAMERA_FACING = FacingDirection.BACK
+    private fun startPreviewInternal(
+        targetViewSize: Size
+    ) {
+        val streamer = streamer ?: run {
+            Logger.w(TAG, "Streamer has not been set")
+            return
+        }
+        if (width == 0 || height == 0) {
+            Logger.w(TAG, "View size is not ready")
+            return
+        }
+        if (previewState != PreviewState.STOPPED) {
+            Logger.w(TAG, "Preview is already running or starting")
+            return
+        }
+        previewState = PreviewState.STARTING
+
+        Logger.d(TAG, "Target view size: $targetViewSize")
+
+        val camera = defaultCameraId ?: streamer.camera
+        Logger.i(TAG, "Starting on camera: $camera")
+
+        val request = createRequest(targetViewSize, camera)
+        viewFinderSurfaceRequest = request
+
+        sendRequest(request, { surface ->
+            post {
+                if (ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    viewFinderSurfaceRequest?.markSurfaceSafeToRelease()
+                    viewFinderSurfaceRequest = null
+                    previewState = PreviewState.STOPPED
+                    Logger.e(
+                        TAG,
+                        "Camera permission is needed to run this application"
+                    )
+                    listener?.onPreviewFailed(SecurityException("Camera permission is needed to run this application"))
+                } else {
+                    streamer.startPreview(surface, camera)
+                    previewState = PreviewState.RUNNING
+                    listener?.onPreviewStarted()
+                }
+            }
+        }, { t ->
+            post {
+                viewFinderSurfaceRequest?.markSurfaceSafeToRelease()
+                viewFinderSurfaceRequest = null
+                previewState = PreviewState.STOPPED
+                Logger.w(TAG, "Failed to get a Surface: $t", t)
+                listener?.onPreviewFailed(t)
+            }
+        })
     }
 
-    private inner class StreamerHolderCallback : SurfaceHolder.Callback {
-        override fun surfaceCreated(holder: SurfaceHolder) {
-            startPreviewIfReady()
+    private fun createRequest(
+        targetViewSize: Size,
+        camera: String,
+    ): ViewfinderSurfaceRequest {
+        /**
+         * Get the closest available preview size to the view size.
+         */
+        val previewSize = getPreviewOutputSize(
+            context.getCameraCharacteristics(camera),
+            targetViewSize,
+            SurfaceHolder::class.java
+        )
+
+        Logger.d(TAG, "Selected preview size: $previewSize")
+
+        val builder = ViewfinderSurfaceRequest.Builder(previewSize)
+        builder.populateFromCharacteristics(context.getCameraCharacteristics(camera))
+
+        return builder.build()
+    }
+
+    private fun sendRequest(
+        request: ViewfinderSurfaceRequest,
+        onSuccess: (Surface) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ) {
+        val surfaceListenableFuture =
+            cameraViewFinder.requestSurfaceAsync(request)
+
+        Futures.addCallback(
+            surfaceListenableFuture,
+            object : FutureCallback<Surface> {
+                override fun onSuccess(surface: Surface) {
+                    onSuccess(surface)
+                }
+
+                override fun onFailure(t: Throwable) {
+                    onFailure(t)
+                }
+            },
+            ContextCompat.getMainExecutor(context)
+        )
+    }
+
+    companion object {
+        private const val TAG = "PreviewView"
+
+        private val DEFAULT_CAMERA_FACING = CameraFacingDirection.BACK
+
+
+        private fun getPosition(scaleType: ScaleType): Position {
+            return when (scaleType) {
+                ScaleType.FILL_START -> Position.START
+                ScaleType.FILL_CENTER -> Position.CENTER
+                ScaleType.FILL_END -> Position.END
+                ScaleType.FIT_START -> Position.START
+                ScaleType.FIT_CENTER -> Position.CENTER
+                ScaleType.FIT_END -> Position.END
+            }
         }
 
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) =
-            Unit
+        private fun getScaleMode(scaleType: ScaleType): ScaleMode {
+            return when (scaleType) {
+                ScaleType.FILL_START -> ScaleMode.FILL
+                ScaleType.FILL_CENTER -> ScaleMode.FILL
+                ScaleType.FILL_END -> ScaleMode.FILL
+                ScaleType.FIT_START -> ScaleMode.FIT
+                ScaleType.FIT_CENTER -> ScaleMode.FIT
+                ScaleType.FIT_END -> ScaleMode.FIT
+            }
+        }
 
-        override fun surfaceDestroyed(holder: SurfaceHolder) {
-            streamer?.stopPreview()
+        private fun getScaleType(scaleMode: ScaleMode, position: Position): ScaleType {
+            when (position) {
+                Position.START -> {
+                    return when (scaleMode) {
+                        ScaleMode.FILL -> ScaleType.FILL_START
+                        ScaleMode.FIT -> ScaleType.FIT_START
+                    }
+                }
+
+                Position.CENTER -> {
+                    return when (scaleMode) {
+                        ScaleMode.FILL -> ScaleType.FILL_CENTER
+                        ScaleMode.FIT -> ScaleType.FIT_CENTER
+                    }
+                }
+
+                Position.END -> {
+                    return when (scaleMode) {
+                        ScaleMode.FILL -> ScaleType.FILL_END
+                        ScaleMode.FIT -> ScaleType.FIT_END
+                    }
+                }
+            }
         }
     }
 
@@ -227,20 +441,122 @@ class PreviewView @JvmOverloads constructor(
     }
 
 
+    /**
+     * A listener for the [PreviewView].
+     */
     interface Listener {
+        /**
+         * Called when the preview is started.
+         */
         fun onPreviewStarted() {}
+
+        /**
+         * Called when the preview failed to start.
+         */
+        fun onPreviewFailed(t: Throwable) {}
+
+        /**
+         * Called when the zoom ratio is changed.
+         * @param zoomRatio the new zoom ratio
+         */
         fun onZoomRationOnPinchChanged(zoomRatio: Float) {}
     }
-}
 
-enum class FacingDirection(val value: String) {
-    FRONT("front"),
-    BACK("back");
+    /**
+     * Options for the camera facing direction.
+     */
+    enum class CameraFacingDirection(val value: Int) {
+        /**
+         * The facing of the camera is the same as that of the screen.
+         */
+        FRONT(0),
 
-    companion object {
-        fun fromValue(value: String): FacingDirection {
-            return entries.first { it.value == value }
+        /**
+         * The facing of the camera is opposite to that of the screen.
+         */
+        BACK(1);
+
+        companion object {
+            /**
+             * Returns the [CameraFacingDirection] from the given id.
+             */
+            internal fun entryOf(value: Int) = entries.first { it.value == value }
         }
     }
-}
 
+    /**
+     * Options for the position of the [PreviewView] within its container.
+     */
+    enum class Position(val value: Int) {
+        /**
+         * The [PreviewView] is positioned at the top of its container.
+         */
+        START(0),
+
+        /**
+         * The [PreviewView] is positioned in the center of its container.
+         */
+        CENTER(1),
+
+        /**
+         * The [PreviewView] is positioned in the bottom of its container.
+         */
+        END(2);
+
+        companion object {
+            /**
+             * Returns the [Position] from the given id.
+             */
+            internal fun entryOf(value: Int) = entries.first { it.value == value }
+        }
+    }
+
+    /**
+     * Options for scaling the [PreviewView] within its container.
+     */
+    enum class ScaleMode(val value: Int) {
+        /**
+         * Scale the [PreviewView], maintaining the source aspect ratio, so it fills the entire
+         * parent.
+         *
+         * This may cause the [PreviewView] to be cropped.
+         */
+        FILL(0),
+
+        /**
+         * Scale the [PreviewView], maintaining the source aspect ratio, so it is entirely contained
+         * within the parent. The background area not covered by the viewfinder stream will be black
+         * or the background of the [PreviewView].
+         *
+         *
+         * Both dimensions of the [PreviewView] will be equal or less than the corresponding
+         * dimensions of its parent.
+         */
+        FIT(1);
+
+        companion object {
+            /**
+             * Returns the [ScaleMode] from the given id.
+             */
+            internal fun entryOf(value: Int) = entries.first { it.value == value }
+        }
+    }
+
+    enum class PreviewState {
+        /**
+         * The preview is stopped.
+         */
+        STOPPED,
+
+        /**
+         * The preview is starting.
+         */
+        STARTING,
+
+        /**
+         * The preview is running.
+         */
+        RUNNING
+    }
+
+}
