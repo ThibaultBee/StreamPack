8c61c9f346c8117a97d94d42e704a0b41f215eb5
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-07-17 12:09:36 +0200

    refactor(*): use Kotlin DSL instead of groovy

---
+++ demos/screenrecorder/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id(libs.plugins.android.application.get().pluginId)
+    alias(libs.plugins.kotlin.android)
+    id(libs.plugins.kotlin.kapt.get().pluginId)
+}
+
+android {
+    namespace = "io.github.thibaultbee.streampack.screenrecorder"
+
+    defaultConfig {
+        applicationId = "io.github.thibaultbee.streampack.screenrecorder"
+
+        minSdk = AndroidVersions.MIN_SDK
+        targetSdk = AndroidVersions.TARGET_SDK
+        compileSdk = AndroidVersions.COMPILE_SDK
+
+        versionCode = extra.get("versionCode") as Int
+        versionName = "$version"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+    packaging {
+        jniLibs {
+            pickFirsts += setOf("**/*.so")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":streampack-core"))
+    implementation(project(":streampack-services"))
+    implementation(project(":streampack-extension-rtmp"))
+    implementation(project(":streampack-extension-srt"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.preference.ktx)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
