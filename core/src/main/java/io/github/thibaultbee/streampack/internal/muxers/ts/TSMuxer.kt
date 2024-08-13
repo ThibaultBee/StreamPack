0710617fb12efd3514da35483d05f9cb971d597f
Välimäki Tuomas <tuomas.valimaki@insta.fi>
2024-08-13 08:14:07 +0300

    Write OPUS control header in TSMuxer.

--- core/src/main/java/io/github/thibaultbee/streampack/internal/muxers/ts/TSMuxer.kt
+++ core/src/main/java/io/github/thibaultbee/streampack/internal/muxers/ts/TSMuxer.kt
@@ -150,14 +150,45 @@ class TSMuxer(
             }
 
             MediaFormat.MIMETYPE_AUDIO_OPUS -> {
-                frame
-            } // TODO: optional control header
+                val payload = frame.rawBuffer
+                val controlHeader = opusControlHeader(payload.remaining())
+                val opusAccessUnit = ByteBuffer.allocate(controlHeader.remaining() + payload.remaining())
+                opusAccessUnit.put(controlHeader)
+                opusAccessUnit.put(payload)
+                opusAccessUnit.rewind()
+                frame.copy(rawBuffer = opusAccessUnit)
+            }
             else -> throw IllegalArgumentException("Unsupported mimeType ${frame.mimeType}")
         }
 
         synchronized(this) {
             generateStreams(newFrame, pes)
         }
+    }
+
+    private fun opusControlHeader(payloadSize: Int): ByteBuffer {
+        val payloadSizeFullBytesCount = payloadSize / 255
+        val payloadSizeRemainderByte = payloadSize % 255
+        val headerSize = 2 + payloadSizeFullBytesCount + payloadSizeRemainderByte.coerceAtMost(1)
+        val header = ByteBuffer.allocate(headerSize)
+
+        // control_header_prefix 11 bits (0x3FF or 01111111111)
+        // start_trim_flag 1 bit (0)
+        // end_trim_flag 1 bit (0)
+        // control_extension_flag 1 bit (0)
+        // reserved 2 bits (0)
+        header.put(0x7F.toByte())
+        header.put(0xE0.toByte())
+
+        repeat(payloadSizeFullBytesCount) {
+            header.put(0xFF.toByte())
+        }
+        if (payloadSizeRemainderByte > 0) {
+            header.put(payloadSizeRemainderByte.toByte())
+        }
+
+        header.rewind()
+        return header
     }
 
     /**
