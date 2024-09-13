5015ab8e55f71217522bacdb0c864326c904cc2c
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-09-13 15:51:45 +0200

    feat(test): add test for streamer

---
+++ core/src/androidTest/java/io/github/thibaultbee/streampack/core/streamer/file/StreamerUtils.kt
@@ -0,0 +1,33 @@
+package io.github.thibaultbee.streampack.core.streamer.file
+
+import io.github.thibaultbee.streampack.core.data.mediadescriptor.MediaDescriptor
+import io.github.thibaultbee.streampack.core.streamers.DefaultStreamer
+import io.github.thibaultbee.streampack.core.streamers.startStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import org.junit.Assert.assertTrue
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+object StreamerUtils {
+    suspend fun runStream(
+        streamer: DefaultStreamer,
+        descriptor: MediaDescriptor,
+        duration: Duration,
+        pollDuration: Duration = 1.seconds
+    ) {
+        streamer.startStream(descriptor)
+        var i = 0
+        val numOfLoop = duration / pollDuration
+        withContext(Dispatchers.Default) {
+            while (i < numOfLoop) {
+                i++
+                delay(pollDuration)
+                assertTrue(streamer.isStreaming.value)
+            }
+        }
+        streamer.stopStream()
+        streamer.close()
+    }
+}
---
+++ core/src/androidTest/java/io/github/thibaultbee/streampack/core/streamer/file/VideoUtils.kt
@@ -0,0 +1,63 @@
+package io.github.thibaultbee.streampack.core.streamer.file
+
+import android.content.Context
+import android.media.MediaMetadataRetriever
+import android.media.MediaMetadataRetriever.METADATA_KEY_DURATION
+import android.media.MediaMetadataRetriever.METADATA_KEY_HAS_AUDIO
+import android.media.MediaMetadataRetriever.METADATA_KEY_HAS_VIDEO
+import android.media.MediaMetadataRetriever.METADATA_KEY_SAMPLERATE
+import android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT
+import android.media.MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH
+import android.net.Uri
+import io.github.thibaultbee.streampack.core.data.AudioConfig
+import io.github.thibaultbee.streampack.core.data.VideoConfig
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import java.io.File
+
+object VideoUtils {
+    fun verifyFile(file: File) {
+        assertTrue(file.exists())
+        assertTrue(file.length() > 0)
+    }
+
+    fun verify(
+        context: Context,
+        uri: Uri,
+        audioConfig: AudioConfig?,
+        videoConfig: VideoConfig?,
+    ) {
+        val metadataRetriever = MediaMetadataRetriever()
+        try {
+            metadataRetriever.setDataSource(context, uri)
+            assertTrue(metadataRetriever.extractMetadata(METADATA_KEY_DURATION)!!.toInt() > 0)
+
+            // Video
+            if (videoConfig != null) {
+                assertEquals("yes", metadataRetriever.extractMetadata(METADATA_KEY_HAS_VIDEO)!!)
+                assertEquals(
+                    videoConfig.resolution.height,
+                    metadataRetriever.extractMetadata(METADATA_KEY_VIDEO_WIDTH)!!.toInt()
+                )
+                assertEquals(
+                    videoConfig.resolution.width,
+                    metadataRetriever.extractMetadata(METADATA_KEY_VIDEO_HEIGHT)!!.toInt()
+                )
+            }
+
+            // Audio
+            if (audioConfig != null) {
+                assertEquals("yes", metadataRetriever.extractMetadata(METADATA_KEY_HAS_AUDIO)!!)
+                assertEquals(
+                    audioConfig.sampleRate,
+                    metadataRetriever.extractMetadata(METADATA_KEY_SAMPLERATE)!!.toInt()
+                )
+            }
+
+        } catch (e: Exception) {
+            throw e
+        } finally {
+            metadataRetriever.release()
+        }
+    }
+}
