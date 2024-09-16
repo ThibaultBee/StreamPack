fafc2b1b1d832a95f24608d79f2a2dc9a4b6edd5
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2024-09-16 11:16:17 +0200

    feat(test): core: add test for sink and endpoint state

---
+++ core/src/androidTest/java/io/github/thibaultbee/streampack/core/internal/endpoints/composites/sinks/SinkStateTest.kt
@@ -0,0 +1,35 @@
+package io.github.thibaultbee.streampack.core.internal.endpoints.composites.sinks
+
+import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * Test class for [ISink] state.
+ */
+@RunWith(Parameterized::class)
+class SinkStateTest(private val endpoint: ISinkInternal) {
+    @Test
+    fun closeTest() = runTest {
+        endpoint.close()
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(
+            name = "Sink: {0}"
+        )
+        fun getMediaDescriptor(): Iterable<ISinkInternal> {
+            val context = InstrumentationRegistry.getInstrumentation().context
+
+            return arrayListOf(
+                FileSink(),
+                ContentSink(context),
+                ChunkedFileOutputStreamSink(1000),
+                FakeSink()
+            )
+        }
+    }
+}
