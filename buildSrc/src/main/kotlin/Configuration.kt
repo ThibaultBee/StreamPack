d65a9dbb44f97b045829231195f70e2711d5e0c4
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2025-07-07 14:23:13 +0200

    chore(deps): upgrade dependencies

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -1,7 +1,7 @@
 object AndroidVersions {
     const val MIN_SDK = 21
-    const val TARGET_SDK = 35
-    const val COMPILE_SDK = 35
+    const val TARGET_SDK = 36
+    const val COMPILE_SDK = 36
 }
 
 object Publication {
