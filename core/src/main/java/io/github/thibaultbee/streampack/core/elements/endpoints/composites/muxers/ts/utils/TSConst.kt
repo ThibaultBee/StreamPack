823130ea9429ba7c01e92eef15b919b5a1b77b97
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2026-09-10 22:24:20 +0200

    fix(core): fix TS clocks computation to avoid an overflow after 94h See https://github.com/ThibaultBee/StreamPack/issues/301

--- core/src/main/java/io/github/thibaultbee/streampack/core/elements/endpoints/composites/muxers/ts/utils/TSConst.kt
+++ core/src/main/java/io/github/thibaultbee/streampack/core/elements/endpoints/composites/muxers/ts/utils/TSConst.kt
@@ -16,6 +16,5 @@
 package io.github.thibaultbee.streampack.core.elements.endpoints.composites.muxers.ts.utils
 
 object TSConst {
-    const val SYSTEM_CLOCK_FREQ = 27000000
     const val BASE_PID = 0x00020.toShort()
 }
